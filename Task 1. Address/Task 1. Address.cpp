ab53691baa095143dc405d7f78dc14cec58c5e51
Sasha Bagley <volkadav99@list.ru>
2022-11-28 18:31:20 +0300

    first commit

---
+++ Task 1. Address/Task 1. Address.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class Address {
+public:
+
+
+    std::string get_output_address(std::string city, std::string address, int home, int flat) {
+        std::stringstream sstm;
+        sstm << city << ", " << address << ", " << home << ", " << flat;
+        return sstm.str();
+        //return city + ", " + address + ", " + static_cast<std::string>(home) + ", " + flat;
+    }
+    Address(std::string city, std::string address, int home, int flat) {
+        this->m_city = city;
+        this->m_address = address;
+        this->m_home = home;
+        this->m_flat = flat;
+    }
+private:
+    std::string m_city = "неизвестно";
+    std::string m_address = "неизвестно";
+    int m_home = 0;
+    int m_flat = 0;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    //создание файла in.txt
+    std::ofstream inFileStack("in.txt");
+    inFileStack << 5 << std::endl << "Москва" << std::endl << "Строителей" << std::endl << "32" << std::endl << "12" << std::endl;
+    inFileStack << "Новокузнецк" << std::endl << "Науки" << std::endl << "5" << std::endl << "16" << std::endl;
+    inFileStack << "Омск" << std::endl << "Пушкина" << std::endl << "2" << std::endl << "13" << std::endl;
+    inFileStack << "Москва" << std::endl << "Арбат" << std::endl << "4" << std::endl << "1" << std::endl;
+    inFileStack << "Новосибирск" << std::endl << "Мира" << std::endl << "85" << std::endl << "64" << std::endl;
+    inFileStack.close();
+
+    
+    std::ifstream inFile("in.txt");
+    std::ofstream outFile("uot.txt");
+    std::string city, street, address;
+    int size = 0, home = 0, flat = 0;
+    inFile >> size;
+    outFile << size << std::endl;
+    Address classAddress(city, street, home, flat);
+    for (int i = 0; i < size; i++) {
+        inFile >> city;
+        inFile >> street;
+        inFile >> home;
+        inFile >> flat;
+        address = classAddress.get_output_address(city, street, home, flat);
+        std::cout << address << std::endl;
+        outFile << address << std::endl;
+    }
+    outFile.close();
+    inFile.close();
+    return 0;
+}
+
---
+++ Task 2. Sorting Addresses/Task 2. Sorting Addresses.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class Address {
+public:
+
+
+    std::string get_output_address(std::string city, std::string address, int home, int flat) {
+        std::stringstream sstm;
+        sstm << city << ", " << address << ", " << home << ", " << flat;
+        return sstm.str();
+    }
+    void sorted(std::string* address, int size) {
+        std::string temp;
+        for (int j = 0; j < size - 1; j++) {
+            for (int i = 0; i < size - 1; i++) {
+                if (address[i].compare(address[i + 1]) > 0) {//если allAddress[i] > allAddress[i + 1]
+                    temp = address[i];
+                    address[i] = address[i + 1];
+                    address[i + 1] = temp;
+                }
+            }
+        }
+    }
+    Address(std::string city, std::string address, int home, int flat) {
+        this->m_city = city;
+        this->m_address = address;
+        this->m_home = home;
+        this->m_flat = flat;
+    }
+private:
+    std::string m_city = "неизвестно";
+    std::string m_address = "неизвестно";
+    int m_home = 0;
+    int m_flat = 0;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    //создание файла in.txt
+    std::ofstream inFileStack("in.txt");
+    inFileStack << 5 << std::endl << "Москва" << std::endl << "Строителей" << std::endl << "32" << std::endl << "12" << std::endl;
+    inFileStack << "Новокузнецк" << std::endl << "Науки" << std::endl << "5" << std::endl << "16" << std::endl;
+    inFileStack << "Омск" << std::endl << "Пушкина" << std::endl << "2" << std::endl << "13" << std::endl;
+    inFileStack << "Москва" << std::endl << "Арбат" << std::endl << "4" << std::endl << "1" << std::endl;
+    inFileStack << "Новосибирск" << std::endl << "Мира" << std::endl << "85" << std::endl << "64" << std::endl;
+    inFileStack.close();
+
+
+    std::ifstream inFile("in.txt");
+    std::string city, street, address;
+    int size = 0, home = 0, flat = 0;
+    inFile >> size;
+    Address classAddress(city, street, home, flat);
+    std::string* allAddress = new std::string[size];
+    std::ofstream outFile("uot.txt");
+    outFile << size << std::endl;
+    for (int i = 0; i < size; i++) {
+        inFile >> city;
+        inFile >> street;
+        inFile >> home;
+        inFile >> flat;
+        allAddress[i] = classAddress.get_output_address(city, street, home, flat);
+    }
+    inFile.close();
+
+    //сортировка
+    classAddress.sorted(allAddress, size);
+
+    //return massive
+    for (int i = 0; i < size; i++) {
+        std::cout << allAddress[i] << std::endl;
+    }
+    for (int i = 0; i < size; i++) {
+        outFile << allAddress[i] << std::endl;
+    }
+
+    delete[] allAddress;
+    outFile.close();
+    return 0;
+}
